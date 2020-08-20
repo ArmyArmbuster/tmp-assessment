a07892bea289d817375f7522795c218d5c781d64
Army Armbuster <army.armbuster@gmail.com>
2020-08-20 18:00:12 +0200

    #3 completed emailWishes controller

--- server/service/schema/controllers/emailWishes.js
+++ server/service/schema/controllers/emailWishes.js
@@ -1,11 +1,66 @@
+/// region imports
+const insertEmployees = require('../../service/insertEmployees');
+const getEmployeeAnniversaries = require('../util/getEmployeeAnniversaries');
+const getEmployeeCollection = require('../util/getEmployeeCollection');
+const getEmployeeExclusions = require('../util/getEmployeeExclusions');
+const generateWish = require('../util/generateWish');
+/// endregion
+
+const transmitWish = (wish) => {
+
+  /// SEND EMAIL WITH NODE MAILER
+  console.log(wish);
+
+  return {
+    message: wish,
+    success: true,
+    code: 'OK',
+  };
+};
+
+const transmitNone = (none = 'No wishes today!') => {
+
+  /// SEND EMAIL WITH NODE MAILER
+  console.log(none);
+
+  return {
+    message: none,
+    success: false,
+    code: 'NA',
+  };
+};
+
 /**
  * exports graphql schema resolver
  * @module graphql schema resolver
  */
-module.exports = function () {
-  return {
-    message: 'OK',
-    success: true,
-    code: 'OK',
-  };
+module.exports = async function (_source, _args, { dataSources }) {
+
+  /// graphql arguments
+  const { message = process.env.ACME_MESSAGE, field = 'dateOfBirth', date = Date.now() } = _args;
+
+  /// graphql context
+  const { ACMEDataSource } = dataSources;
+
+  /// obtain employee collections
+  const employeeCollection = await getEmployeeCollection(ACMEDataSource);
+  const employeeExclusions = await getEmployeeExclusions(ACMEDataSource);
+
+  /// obtain employees
+  const employees = getEmployeeAnniversaries(employeeCollection, employeeExclusions, field, new Date(date));
+
+  if (employees.length) {
+
+    /// obtain employees' wish
+    const wish = generateWish(message, employees);
+
+    /// insert employees wished
+    await insertEmployees(employees.map((employee) => {
+      return { _id: employee.id };
+    }));
+
+    return transmitWish(wish);
+  } else {
+    return transmitNone();
+  }
 };
