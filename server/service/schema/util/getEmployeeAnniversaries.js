d1a4a44a7440b709c121ac5549877750787133e8
Army Armbuster <army.armbuster@gmail.com>
2020-08-20 16:16:43 +0200

    #3 formatted employee prior processing

--- server/service/schema/util/getEmployeeAnniversaries.js
+++ server/service/schema/util/getEmployeeAnniversaries.js
@@ -4,12 +4,23 @@ const isEmployed = require('./isEmployed');
 const isIncluded = require('./isIncluded');
 /// endregion
 
+const formatEmployee = (employee, field) => {
+
+  employee.employmentStartDate = new Date(employee.employmentStartDate);
+  employee.employmentEndDate = new Date(employee.employmentEndDate);
+  employee[`${field}`] = new Date(employee[`${field}`]);
+
+  return employee;
+};
+
 /**
  * exports anniversary filter function
  * @module anniversary filter function
  */
 module.exports = function (employees, exclusions, field, now = new Date()) {
   return employees
+    /// format employee
+    .map((employee) => formatEmployee(employee, field))
     /// filter employee dates
     .filter(({ employmentStartDate, employmentEndDate }) => isEmployed(employmentStartDate, employmentEndDate, now))
     /// filter employee exclusions
